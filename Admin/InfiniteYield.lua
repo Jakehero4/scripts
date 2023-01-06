595d02712b0d1c7ea107bce7ada2dab2029c2bfd
Jakehero4 <110546700+Jakehero4@users.noreply.github.com>
2023-01-05 18:22:56 -0800

    Update InfiniteYield.lua

--- Admin/InfiniteYield.lua
+++ Admin/InfiniteYield.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Jakehero4/scripts/main/infinite-yield", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source", true))()
