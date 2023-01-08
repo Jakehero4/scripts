f267d94da61f8212ac424af164ee154e49aa8bc0
Jakehero4 <110546700+Jakehero4@users.noreply.github.com>
2023-01-08 11:36:43 -0800

    Update Sword Fight Bot.lua

--- Global/Sword Fight Bot.lua
+++ Global/Sword Fight Bot.lua
@@ -1,3 +1,2 @@
--- press x to turn on
+-- X To Enable
 loadstring(game:HttpGet('https://raw.githubusercontent.com/yeerma/1/main/bot'))()
---credits: https://v3rmillion.net/showthread.php?tid=1197208
